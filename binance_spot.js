2fc4a77f980f9c7be924b01025dc0ed7d965a35f
Byungin Song <byunginhb@gmail.com>
2024-05-29 22:49:09 +0900

    edit: 로직 수정

--- binance_spot.js
+++ binance_spot.js
@@ -11,8 +11,8 @@ const binance = new Binance().options({
 });
 
 // 매수 및 매도 조건 설정
-const rsiBuyThreshold = 33; // RSI 과매도 조건
-const rsiSellThreshold = 60; // RSI 과매수 조건
+const rsiBuyThreshold = 30; // RSI 과매도 조건
+const rsiSellThreshold = 65; // RSI 과매수 조건
 
 // 손절, 손익 조건
 const stopLossPercent = -5; // 손절 조건
