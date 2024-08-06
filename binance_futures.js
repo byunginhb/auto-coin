45c165e66827d96c3b3d8d1991f8358e98fe8524
ben.s <ben.s@kakaocorp.com>
2024-08-06 12:23:27 +0900

    edit: trade 시간 조절

--- binance_futures.js
+++ binance_futures.js
@@ -450,8 +450,8 @@ async function startTrade(symbol = 'BTCUSDT', interval = '15m') {
 
     trade(symbol, interval);
 
-    // 15초마다 trade 함수 실행
-    intervalHandler = setInterval(() => trade(symbol, interval), 15 * 1000);
+    // 300초마다 trade 함수 실행
+    intervalHandler = setInterval(() => trade(symbol, interval), 300 * 1000);
     sendMessage('트레이딩을 시작합니다.');
   } catch (error) {
     sendMessage('Trade execution start failed:', error);
